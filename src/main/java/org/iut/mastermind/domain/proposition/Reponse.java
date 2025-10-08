c4e029ca4c2aa181764a0fa98d7669027cb0997a
korban2u <ryankorban2004@gmail.com>
2025-10-08 17:16:58 +0200

    fix Reponse.java

--- src/main/java/org/iut/mastermind/domain/proposition/Reponse.java
+++ src/main/java/org/iut/mastermind/domain/proposition/Reponse.java
@@ -15,18 +15,26 @@ public class Reponse {
 
     // on récupère la lettre à la position spécifiée en paramètre dans le résultat
     public Lettre lettre(int position) {
-        return null;
+        return resultat.get(position);
     }
 
     // on construit le résultat en analysant chaque lettre
     // du mot proposé
     public void compare(String essai) {
         // TBD
+        resultat.clear();
+        for (int i = 0; i < essai.length(); i++) {
+            position = i;
+            char carCourant = essai.charAt(i);
+            Lettre evaluation = evaluationCaractere(carCourant);
+            resultat.add(evaluation);
+        }
+
     }
 
     // vrai si toutes les lettres sont placées
     public boolean lettresToutesPlacees() {
-        return false;
+        return resultat.stream().allMatch(res -> res.equals(Lettre.PLACEE));
     }
 
     public List<Lettre> lettresResultat() {
@@ -35,16 +43,22 @@ public class Reponse {
 
     // renvoie le statut du caractère (incorrect, mal placé, placé)
     private Lettre evaluationCaractere(char carCourant) {
-        return null;
+        if (estPlace(carCourant))
+            return Lettre.PLACEE;
+
+        if (estPresent(carCourant))
+            return Lettre.NON_PLACEE;
+
+        return Lettre.INCORRECTE;
     }
 
     // vrai si le caractère est présent dans le mot secret
     private boolean estPresent(char carCourant) {
-        return false;
+        return motSecret.chars().anyMatch(c -> c == carCourant);
     }
 
     // vrai si le caractère est placé dans le mot secret
     private boolean estPlace(char carCourant) {
-        return false;
+        return motSecret.charAt(position) == carCourant;
     }
 }
